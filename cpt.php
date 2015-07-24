e32e4c0e10855e6318f2ca4f9c0b4e778886d6d1
Benjam Welker <benjamwelker@gmail.com>
2015-07-24 01:36:30 -0600

    add plugin file

---
+++ cpt.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package CPT
+ * @version 1.0
+ */
+/*
+Plugin Name: Custom Post Types Instatiator
+Plugin URI: https://github.com/benjamw/custom_post_types
+Description: Instatiates the CPTs included in this installation
+Author: Benjam Welker
+Version: 1.0
+Author URI: http://iohelix.net/
+*/
+
+include 'custom_post_type.class.php';
