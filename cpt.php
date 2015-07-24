c535e686ab193800dc8b798c9d0f39c1ecf4ef17
Benjam Welker <benjamwelker@gmail.com>
2015-07-24 01:47:15 -0600

    don't break when activating plugin

--- cpt.php
+++ cpt.php
@@ -12,4 +12,19 @@ Version: 1.0
 Author URI: http://iohelix.net/
 */
 
-include 'custom_post_type.class.php';
+function include_cpt_files( ) {
+	// open the current dir
+	$dh = opendir(dirname(__FILE__));
+
+	$filelist = array( );
+	while (false !== ($file = readdir($dh))) {
+		if (preg_match('/^cpt\..+\.php$/i', $file)) { // scanning for cpt.__.php files only
+			// if we found one of those files, include it, the rest happens by magic
+			include $file;
+		}
+	}
+
+	closedir($dh);
+}
+
+include_once 'custom_post_type.class.php';
